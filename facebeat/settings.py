4faea003de81c592f9978c53d5db9ab8e9ae3fd5
adarsh-varrier <adarshpmanimandiram@gmail.com>
2024-10-07 13:53:37 +0530

    latest

--- facebeat/settings.py
+++ facebeat/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'facebeat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,6 +125,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+
+    os.path.join(BASE_DIR, "static")
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
